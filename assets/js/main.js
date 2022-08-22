c993be0ab5a25cd0ce1f6aab74b17c4e226299dc
AYUSH PATNI <ayushdpatni@gmail.com>
2022-08-23 00:54:51 +0530

    Intermediate Level

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 /*=============== SHOW SIDEBAR ===============*/
 
+const navMenu = document.getElementById('sidebar'),
+navToggle=document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
 
 /*===== SIDEBAR SHOW =====*/
 /* Validate If Constant Exists */
-
-
+if(navToggle){
+    navToggle.addEventListener("click",()=>{
+        navMenu.classList.add('show-sidebar')
+    })
+}
 /*===== SIDEBAR HIDDEN =====*/
 /* Validate If Constant Exists */
-
+if(navClose){
+    navClose.addEventListener("click",()=>{
+        navMenu.classList.remove('show-sidebar')
+    })
+}
 
 /*=============== SKILLS TABS ===============*/
 const tabs = document.querySelectorAll('[data-target]');
@@ -106,8 +116,43 @@ let swiper = new Swiper(".testimonials__container", {
 
 /*=============== INPUT ANIMATION ===============*/
 
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc(){
+    let parent = this.parentNode;
+    parent.classList.add("focus")
+}
+
+function blurFunc(){
+    let parent = this.parentNode;
+    if(this.value==""){
+        parent.classList.remove("focus");
+    }
+}
+
+inputs.forEach((input)=>{
+    input.addEventListener("focus",focusFunc);
+    input.addEventListener("blur",blurFunc);
+})
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+const sections = document.querySelectorAll("section[id]");
+window.addEventListener("scroll",navHighlighter);
+function navHighlighter()
+{
+    let scrollY=window.pageYOffset;
+    sections.forEach(current=>{
+        const sectionHeight=current.offsetHeight;
+        const sectionTop=current.offsetTop - 50,
+        sectionId=current.getAttribute("id");
+        if(scrollY>sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add("active-link")
+        }
+        else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove("active-link")
+        }
+    })
+}
 
 /*=============== SHOW SCROLL UP ===============*/
